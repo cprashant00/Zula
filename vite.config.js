2057745af45e97b8114152f6280acd6d4aa9446c
Prashant Chaturvedi <103244022+cprashant00@users.noreply.github.com>
2026-01-04 23:07:42 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Zula/', // Add this line here
   server: {
-    host: true, // Allows external access
+    host: true,
     port: 3000,
   },
 })
